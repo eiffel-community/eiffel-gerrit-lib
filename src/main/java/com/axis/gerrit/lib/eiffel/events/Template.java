868635f6492cd86932ebf8dddc377d8b49986bea
Christian Bilevits <christian.bilevits@gmail.com>
2019-07-19 11:45:03 +0200

    Update license and author

--- src/main/java/com/axis/gerrit/lib/eiffel/events/Template.java
+++ src/main/java/com/axis/gerrit/lib/eiffel/events/Template.java
@@ -1,7 +1,18 @@
 /*
- * Copyright (c) 2019, Axis Communications AB. All rights reserved.
+ * Copyright 2019 Axis Communications AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package com.axis.gerrit.lib.eiffel.events;
 
 
@@ -9,7 +20,7 @@ import com.ericsson.eiffel.semantics.events.Event;
 import com.google.gson.JsonObject;
 
 /**
- * @author christbi, Christian Bilevits
+ * @author Christian Bilevits, christian.bilevits@axis.com
  * @since 2019-07-17
  */
 public interface Template {
