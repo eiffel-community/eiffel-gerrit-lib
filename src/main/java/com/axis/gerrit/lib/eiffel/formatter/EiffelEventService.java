904b0112fb53c32ba0c1b4c685d593faa3583064
Christian Bilevits <christian.bilevits@gmail.com>
2019-07-19 11:48:05 +0200

    Update license and author

--- src/main/java/com/axis/gerrit/lib/eiffel/formatter/EiffelEventService.java
+++ src/main/java/com/axis/gerrit/lib/eiffel/formatter/EiffelEventService.java
@@ -1,5 +1,17 @@
 /*
- * Copyright (c) 2019, Axis Communications AB. All rights reserved.
+ * Copyright 2019 Axis Communications AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.axis.gerrit.lib.eiffel.formatter;
 
@@ -17,8 +29,8 @@ import static com.axis.gerrit.lib.eiffel.formatter.GerritEventType.CHANGE_MERGED
 import static com.axis.gerrit.lib.eiffel.formatter.GerritEventType.PATCHSET_CREATED;
 
 /**
- * @author chrillebile, Christian Bilevits
- * @since 2019-07-09
+ * @author Christian Bilevits, christian.bilevits@axis.com
+ * @since 2019-07-17
  */
 public class EiffelEventService {
 
